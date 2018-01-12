baec7e20772adcea114b4af884c7f753e8bad38d
JAreina <jareinafdez@gmail.com>
2018-01-12 20:40:30 +0100

    inicio angularjs practicas

---
+++ 0-servidor-express/server.js
@@ -0,0 +1,8 @@
+var express = require('express'),
+app
+= express();
+app.use(express. static (__dirname+'/'))
+.get('*', function (req, res){
+res.sendFile('./index/index.html', {root:__dirname});
+}).listen(9999);
+console.log("SERVIDOR http://localhost:9999")
---
+++ 2-ng-repeat/js/app.js
@@ -0,0 +1,42 @@
+(function(){
+
+var app = angular.module('universidadApp',[ ]);
+
+app.controller('listadoCtrl', ['$scope', function($scope){
+
+
+    $scope.listado = ["fernando",
+                       "maria",
+                       "eva"];
+
+    $scope.listadoProfesores = {
+        profesores : [{
+            nombre: "juan",
+            edad: 444,
+            clase: "PRIMERO"
+        },
+        {
+             nombre: "MARIA",
+            edad: 34,
+            clase: "SEGUNDO"
+        }]
+    }
+
+
+   $scope.counter=100;
+   $scope.array=[0];
+
+
+   $scope.updateModel = function () {
+
+		 
+      $scope.array=[];
+      for (var i=0;i<$scope.counter;i++){
+         $scope.array.push(i);
+
+      }
+   };
+
+}]);
+
+})();
