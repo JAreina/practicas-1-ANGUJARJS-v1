df32772ede2a91559b954391b40579a689d41064
JAreina <jareinafdez@gmail.com>
2018-01-13 19:21:32 +0100

    filter personalizado pretty json

--- 7-http-request/js/app.js
+++ 7-http-request/js/app.js
@@ -1,20 +1,31 @@
 (function(){
-
+"use strict";
 var app = angular.module('ejemplosApp',[ ]);
 
 
-
+// inyección de '$http'
 app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
-  
-  
+
+$scope.personas=[];
+
+$http.get('./json/personas.json').success((data)=>{
+  $scope.personas = data.personas;
+})
 
 
-  
+
 
 
 
 }]);
 
+
+app.filter('prettyJSON', function () {
+    function prettyPrintJson(json) {
+      return JSON ? JSON.stringify(json, null, '  ') : 'your browser doesnt support JSON so cant pretty print';
+    }
+    return prettyPrintJson;
+});
 
 
 
