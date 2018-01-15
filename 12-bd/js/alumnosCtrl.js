b2e149c16b1582f703644778c54ffd9ef753a4f2
JAreina <jareinafdez@gmail.com>
2018-01-15 10:47:26 +0100

    controles de navegación en la tabla

--- 12-bd/js/alumnosCtrl.js
+++ 12-bd/js/alumnosCtrl.js
@@ -3,8 +3,26 @@ app.controller('alumnosCtrl', ['$scope','$http', function($scope,$http){
 	$scope.setActive("mAlumnos");
 
 $scope.alumnos = {};
+//mostrar cinco registros
+
+$scope.posicion = 5;
+
 
 $http.get('php/servicios/alumnos.listado.php').success(function(data){
 	$scope.alumnos = data;
-})
+});
+
+//ver siguientes registros
+
+$scope.siguientes = function(){
+	if($scope.alumnos.length > $scope.posicion){
+		$scope.posicion +=5;
+	}
+}
+$scope.anteriores = function(){
+	if( $scope.posicion>5){
+		$scope.posicion -=5;
+	}
+}
+
 }]);
