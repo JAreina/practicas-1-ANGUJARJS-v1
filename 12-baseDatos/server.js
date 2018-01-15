0c5cdb1bc8cfc5dbc2045b1b32a7515ea2886c26
JAreina <jareinafdez@gmail.com>
2018-01-15 15:19:13 +0100

    borr

--- 12-baseDatos/server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express'),
-  app = express();
-app.use(express.static(__dirname + '/'))
-  .get('*', function(req, res) {
-    res.sendFile('/index.html', {
-      root: __dirname
-    });
-  }).listen(9999);
